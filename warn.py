d7b1949a6470967694c88c9212f8291bf7a3c52e
Dave(Munseong Kim) <iris4865@users.noreply.github.com>
2020-08-02 14:44:52 +0900

    Update warn.py

--- warn.py
+++ warn.py
@@ -1,4 +1,8 @@
 import os
 
-value = os.getenv('secrets.TEST_KEY')
-print(value)
+print('start')
+secrets_test_key = os.getenv('secrets.TEST_KEY')
+print(f'stk: secrets_test_key')
+test_key = os.getenv('TEST_KEY')
+print(f'tk: test_key')
+print('end')
